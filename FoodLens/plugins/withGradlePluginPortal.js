2f609f071c0a3b4469fecf6c3bb0ea967adc0ef0
JEONG HYEON OH <52736095+zzocojoa@users.noreply.github.com>
2026-04-03 22:10:52 +0900

    build(android): persist buildscript plugin portal

--- FoodLens/plugins/withGradlePluginPortal.js
+++ FoodLens/plugins/withGradlePluginPortal.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { withDangerousMod } = require('expo/config-plugins');
 
 const SETTINGS_GRADLE_PATH = ['settings.gradle'];
+const BUILD_GRADLE_PATH = ['build.gradle'];
 
 const SETTINGS_REPOSITORIES_BLOCK = `pluginManagement {
   repositories {
@@ -31,11 +32,41 @@ const updateSettingsGradle = async (platformProjectRoot) => {
   await fs.writeFile(settingsGradlePath, updatedContents, 'utf8');
 };
 
+const ensureBuildscriptRepositories = (contents) => {
+  if (contents.includes('gradlePluginPortal()')) {
+    return contents;
+  }
+
+  const buildscriptRepositoriesAnchor = `buildscript {
+  repositories {
+`;
+
+  if (!contents.includes(buildscriptRepositoriesAnchor)) {
+    throw new Error('Could not find buildscript repositories block in android/build.gradle.');
+  }
+
+  return contents.replace(
+    buildscriptRepositoriesAnchor,
+    `buildscript {
+  repositories {
+    gradlePluginPortal()
+`
+  );
+};
+
+const updateBuildGradle = async (platformProjectRoot) => {
+  const buildGradlePath = path.join(platformProjectRoot, ...BUILD_GRADLE_PATH);
+  const contents = await fs.readFile(buildGradlePath, 'utf8');
+  const updatedContents = ensureBuildscriptRepositories(contents);
+  await fs.writeFile(buildGradlePath, updatedContents, 'utf8');
+};
+
 const withGradlePluginPortal = (config) =>
   withDangerousMod(config, [
     'android',
     async (modConfig) => {
       await updateSettingsGradle(modConfig.modRequest.platformProjectRoot);
+      await updateBuildGradle(modConfig.modRequest.platformProjectRoot);
       return modConfig;
     },
   ]);
